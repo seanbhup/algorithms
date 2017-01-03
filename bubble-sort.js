bed2504cbbd0091243fa12d6674c9ee3ff83a93c
Sean Bhupathi <seanbhupathi@gmail.com>
2017-01-02 19:08:25 -0500

    js

--- bubble-sort.js
+++ bubble-sort.js
@@ -2,7 +2,7 @@ function bubbleSort(items){
 	var length = items.length; //SIZE OF N
 	for(let i = 0; i < length; i++){ //FIRST LOOP, THE TOTAL NUMBER OF PASSES
 		var sorted = true; //ASSUME THE LIST IS SORTED
-		for(let j = 0; j < i; j++){
+		for(let j = 0; (j < length - i); j++){
 			//COMPARE THE CURRENT SPOT WITH THE NEXT SPOT
 			if(items[j] > items[j+1]){
 				sorted = false; //WE HAD TO SWITCH TWO NUMBERS, WE DON'T KNOW IF THE LIST IS SORTED ANYMORE
@@ -16,10 +16,10 @@ function bubbleSort(items){
 		break; //STOP THE OUTER LOOP BECAUSE WE DIDNT SWAP ANYTHING ON THIS PASS
 		}
 	}
-	
+	console.log(items);	
 }
-	console.log(items);
+	
 
 
-bubbleSort([1,2,3,4,5,6,7,8]);
-bubbleSort([5,4,2,7,6,8,1,3]);
+bubbleSort([1,2,3,4,5,6,7,8,9]);
+bubbleSort([5,4,2,7,6,8,1,3,12]);
